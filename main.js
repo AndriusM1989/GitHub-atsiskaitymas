113a3705aa6ac27f65e80e65321bec9f7b78e9b5
AndriusM1989 <andrius.morkus@gmail.com>
2023-11-07 14:55:13 +0200

    sutrumpintas javascript kodas

--- main.js
+++ main.js
@@ -1,69 +1,47 @@
-document.getElementById("pirmasMygtukas").addEventListener("click", (e) => {
+document.getElementById("mygtukai").addEventListener("click", (e) => {
   e.preventDefault();
-  document.getElementById("intro").style.display = "flex";
   document.getElementById("pirmaSekcija").style.display = "none";
   document.getElementById("antraSekcija").style.display = "none";
   document.getElementById("treciaSekcija").style.display = "none";
   document.getElementById("mygtukai").style.display = "none";
-  document.querySelector("#intro>div>img").addEventListener('click', () => {
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("pirmaSekcija").style.display = "flex";
-    document.getElementById("antraSekcija").style.display = "flex";
-    document.getElementById("treciaSekcija").style.display = "flex";
-    document.getElementById("mygtukai").style.display = "flex";
-  })
-});
-
-document
-  .getElementById("antrasMygtukas")
-  .addEventListener("click", (e) => {
-    e.preventDefault();
+  if (e.target.id == "pirmasMygtukas") {
+    document.getElementById("intro").style.display = "flex";
+    document.querySelector("#intro>div>img").addEventListener("click", () => {
+      document.getElementById("intro").style.display = "none";
+      document.getElementById("pirmaSekcija").style.display = "flex";
+      document.getElementById("antraSekcija").style.display = "flex";
+      document.getElementById("treciaSekcija").style.display = "flex";
+      document.getElementById("mygtukai").style.display = "flex";
+    });
+  } else if (e.target.id == "antrasMygtukas") {
     document.getElementById("work").style.display = "flex";
-    document.getElementById("pirmaSekcija").style.display = "none";
-    document.getElementById("antraSekcija").style.display = "none";
-    document.getElementById("treciaSekcija").style.display = "none";
-    document.getElementById("mygtukai").style.display = "none";
-    document.querySelector("#work>div>img").addEventListener('click', () => {
+    document.querySelector("#work>div>img").addEventListener("click", () => {
       document.getElementById("work").style.display = "none";
       document.getElementById("pirmaSekcija").style.display = "flex";
       document.getElementById("antraSekcija").style.display = "flex";
       document.getElementById("treciaSekcija").style.display = "flex";
       document.getElementById("mygtukai").style.display = "flex";
-    })
-  });
-
-document
-  .getElementById("treciasMygtukas")
-  .addEventListener("click", (e) => {
-    e.preventDefault();
+    });
+  } else if (e.target.id == "treciasMygtukas") {
     document.getElementById("about").style.display = "flex";
-    document.getElementById("pirmaSekcija").style.display = "none";
-    document.getElementById("antraSekcija").style.display = "none";
-    document.getElementById("treciaSekcija").style.display = "none";
-    document.getElementById("mygtukai").style.display = "none";
-    document.querySelector("#about>div>img").addEventListener('click', () => {
+    document.querySelector("#about>div>img").addEventListener("click", () => {
       document.getElementById("about").style.display = "none";
       document.getElementById("pirmaSekcija").style.display = "flex";
       document.getElementById("antraSekcija").style.display = "flex";
       document.getElementById("treciaSekcija").style.display = "flex";
       document.getElementById("mygtukai").style.display = "flex";
-    })
-  });
-
-document
-  .getElementById("ketvirtasMygtukas")
-  .addEventListener("click", (e) => {
-    e.preventDefault();
+    });
+  } else if (e.target.id == "ketvirtasMygtukas"){
     document.getElementById("contact").style.display = "flex";
-    document.getElementById("pirmaSekcija").style.display = "none";
-    document.getElementById("antraSekcija").style.display = "none";
-    document.getElementById("treciaSekcija").style.display = "none";
-    document.getElementById("mygtukai").style.display = "none";
-    document.querySelector("#contact>div>img").addEventListener('click', () => {
+    document.querySelector("#contact>div>img").addEventListener("click", () => {
       document.getElementById("contact").style.display = "none";
       document.getElementById("pirmaSekcija").style.display = "flex";
       document.getElementById("antraSekcija").style.display = "flex";
       document.getElementById("treciaSekcija").style.display = "flex";
       document.getElementById("mygtukai").style.display = "flex";
-    })
-  });
+    });
+  } else {
+    console.log("Kažkas sugedo");
+  }
+})
+
