0e2f535b92fd3a3ccceba748784dceabe6e82332
AndriusM1989 <andrius.morkus@gmail.com>
2023-11-07 14:00:52 +0200

    Sucentruoti langai

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
-document
-  .getElementById("pirmasMygtukas")
-  .addEventListener("click", (e) => {
-    e.preventDefault();
-    document.getElementById("intro").style.display = "flex";
-    e.target.reset();
+document.getElementById("pirmasMygtukas").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("intro").style.display = "flex";
   });
 
   document
   .getElementById("antrasMygtukas")
   .addEventListener("click", function () {
     document.getElementById("work").style.display = "flex";
-    e.target.reset();
   });
 
   document
   .getElementById("treciasMygtukas")
   .addEventListener("click", function () {
     document.getElementById("about").style.display = "flex";
-    e.target.reset();
   });
 
   document
   .getElementById("ketvirtasMygtukas")
   .addEventListener("click", function () {
     document.getElementById("contact").style.display = "flex";
-    e.target.reset();
   });
